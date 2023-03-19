60154add7ac54d6142148bd1ff6208a44512a493
soojin0925 <happysoo81@naver.com>
2023-03-19 10:06:03 +0900

    2023.03.18 자바1일

---
+++ src/print/Print3.java
@@ -0,0 +1,13 @@
+package print;
+
+public class Print3 {
+	public static void main(String[] args) {
+		/*
+		 * 
+		 */
+		System.out.println("이름: 홍길동");
+		System.out.println("생일 : 2000년 7월 15일");
+		System.out.println("키 : 175.5cm");
+		
+	}
+}
